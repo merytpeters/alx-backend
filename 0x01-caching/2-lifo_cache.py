3442fe9334ae338b49e09daacbf203738aa3ea33
merytpeters <merytpeters@gmail.com>
2024-10-30 10:08:20 +0100

    LIFO caching

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""Subclass LIFOCache"""
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """Subclass of BaseCaching"""
+    def __init__(self):
+        """Initialization"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Puts items in the dictionary"""
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            # Check if the cache exceeds the maximum
+            recent_key = next(reversed(self.cache_data))
+            print(f"DISCARD: {recent_key}")
+            del self.cache_data[recent_key]
+
+        # Add new item to the cache
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Gets item from the cached dictionary"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
